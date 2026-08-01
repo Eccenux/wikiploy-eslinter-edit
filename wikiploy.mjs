fa32e97f05e73bdf0d0d1c773ca1dca379497bc9
eccenux <eccenux+git@gmail.com>
2026-08-01 16:37:03 +0200

    cleanup - deps up - README sync with edits branch - wikiploy lanucher from edits branch

--- wikiploy.mjs
+++ wikiploy.mjs
@@ -13,10 +13,12 @@ ployBot.site = "pl.wikipedia.org";
 
 (async () => {
 	// can use Windows or Linux style
-	let file = String.raw`src\commons.wikimedia.org\MediaWiki.ns.PermissionsRequestsDesk.js`;
-
 	const configs = [];
-	addPloyConfig(configs, file);
+	/**
+	addPloyConfig(configs, String.raw`src\commons.wikimedia.org\MediaWiki.ns.PermissionsRequestsDesk.js`);
+	addPloyConfig(configs, String.raw`src\commons.wikimedia.org\MediaWiki.ns.Gadget-Cat-a-lot.js`);
+	/**/
+
 
 	// custom summary
 	let summary = 'change from Github'; // default
